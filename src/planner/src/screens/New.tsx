17612529b823478e9f75f0531c009a21fb6455b3
Lenaro <100224127+LenaroT@users.noreply.github.com>
2023-04-27 09:06:22 -0300

    Update New.tsx

--- src/planner/src/screens/New.tsx
+++ src/planner/src/screens/New.tsx
@@ -1,103 +1,136 @@
 import React, { useState } from 'react';
-import { ScrollView, Text, View, TextInput, TouchableOpacity, StyleSheet } from "react-native";
-import { Checkbox, } from 'react-native-paper';
-import type { CheckboxProps } from 'react-native-paper';
+import { ScrollView, Text, View, TextInput, TouchableOpacity, StyleSheet, } from "react-native";
+import { useRoute } from '@react-navigation/native';
 import { BackButtom } from "../components/BackButtom";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { AntDesign } from '@expo/vector-icons';
+import { CheckBox } from "../components/CheckBox";
+import dayjs from "dayjs";
 
-
-
+interface Params {
+    date: string;
+    checked: boolean;
+}
 interface Props {
     onPress: () => void;
 }
 
+export function New() {
 
+    const [dia, setDia] = useState([
+        { title: 'Domingo', checked: false },
+        { title: 'Segunda-feira', checked: false },
+        { title: 'Terça-feira', checked: false },
+        { title: 'Quarta-feira', checked: false },
+        { title: 'Quinta-feira', checked: false },
+        { title: 'Sexta-feira', checked: false },
+        { title: 'Sábado', checked: false },
 
-const WeekDaysCheckbox = () => {
+      ]);
 
-    const [weekDays, setWeekDays] = useState([
-        { label: 'Domingo', checked: false },
-        { label: 'Segunda', checked: false },
-        { label: 'Terça', checked: false },
-        { label: 'Quarta', checked: false },
-        { label: 'Quinta', checked: false },
-        { label: 'Sexta', checked: false },
-        { label: 'Sábado', checked: false },
-    ]);
-    const handleCheckboxChange = (index: number) => {
-        const updatedWeekDays = [...weekDays];
-        updatedWeekDays[index].checked = !updatedWeekDays[index].checked;
-        setWeekDays(updatedWeekDays);
+    const handleCheckBoxChange = (index: number) => {
+        const updatedDiaSemana = [...dia];
+        updatedDiaSemana[index].checked = !updatedDiaSemana[index].checked;
+        setDia(updatedDiaSemana);
     };
+
+
     const [value, onChangeText] = useState('')
 
     return (
 
-        <View className="flex-1 bg-background   ">
-            <View className=" px-8 pt-16">
-                <ScrollView showsVerticalScrollIndicator={false}>
+        <View className="flex-1 bg-background px-8 pt-16  ">
+            <ScrollView showsVerticalScrollIndicator={false}>
 
-                    <BackButtom />
+                <BackButtom />
 
-                    <Text className="text-white mt-6 font-extrabold text-3xl">
-                        Criar hábito
-                    </Text>
+                <Text className="text-white mt-6 font-extrabold text-3xl">
+                    Criar hábito
+                </Text>
 
-                    <Text className="text-white mt-6 font-semibold text-base">
-                        Qual seu comprometimento?
-                    </Text>
-                    <View className="text-zinc-400" >
+                <Text className="text-white mt-6 font-semibold text-base">
+                    Qual seu comprometimento?
+                </Text>
+                <View className="text-zinc-400" >
 
-                        <TextInput
+                    <TextInput
 
-                            className="border border-zinc-800 font-extrabold  rounded-lg "
-                            numberOfLines={1}
-                            maxLength={40}
-                            onChangeText={text => onChangeText(text)}
-                            value={value}
-                            style={styles.TextInput}
-                            placeholder="Exercicios, dormir bem etc..."
-                            placeholderTextColor="#A1A1AA"
-                        />
+                        className="border border-zinc-800 font-extrabold  rounded-lg "
+                        numberOfLines={1}
+                        maxLength={40}
+                        onChangeText={text => onChangeText(text)}
+                        value={value}
+                        style={styles.TextInput}
+                        placeholder="Exercícios, dormir bem, etc..."
+                        placeholderTextColor="#A1A1AA"
+                    />
 
-                    </View>
+                </View>
+                <View>
 
-                    <Text className="text-white mt-6 font-semibold text-base">
+                    <Text className="text-white mt-4 mb-6 font-semibold text-base">
                         Qual a Recorrência?
                     </Text>
 
-                    <View >
-                        {weekDays.map((day, index) => (
-                            <View key={index} style={{ flexDirection: 'row', alignItems: 'center' }}>
+                </View>
+                <View>
 
-                                <Checkbox
+                    <CheckBox
+                        checked={dia[0].checked}
+                        title={dia[0].title}
+                        onPress={() => handleCheckBoxChange(0)}
+                    />
+                    <CheckBox
+                        checked={dia[1].checked}
+                        title={dia[1].title}
+                        onPress={() => handleCheckBoxChange(1)}
+                    />
+                    <CheckBox
+                        checked={dia[2].checked}
+                        title={dia[2].title}
+                        onPress={() => handleCheckBoxChange(2)}
+                    />
+                    <CheckBox
+                        checked={dia[3].checked}
+                        title={dia[3].title}
+                        onPress={() => handleCheckBoxChange(3)}
+                    />
+                    <CheckBox
+                        checked={dia[4].checked}
+                        title={dia[4].title}
+                        onPress={() => handleCheckBoxChange(4)}
+                    />
+                    <CheckBox
+                        checked={dia[5].checked}
+                        title={dia[5].title}
+                        onPress={() => handleCheckBoxChange(5)}
+                    />
+                    <CheckBox
+                        checked={dia[6].checked}
+                        title={dia[6].title}
+                        onPress={() => handleCheckBoxChange(6)}
+                    />
 
-                                    status={day.checked ? 'checked' : 'unchecked'}
-                                    onPress={() => handleCheckboxChange(index)}
-                                    color={day.checked ? 'green' : 'white'}
+                </View>
 
-                                />
-                                <Text className="text-white">{day.label}</Text>
-                            </View>
-                        ))}
-                    </View>
+                <View >
                     <TouchableOpacity style={styles.button} >
                         <View style={{ flexDirection: 'row', alignItems: 'center' }} >
-                            <Icon name="check" size={10} color="white" />
+                            <AntDesign name="check" size={13} color="white" />
 
                             <Text style={styles.text}>  Confirmar</Text>
                         </View>
                     </TouchableOpacity>
 
+                </View>
 
-
-
-                </ScrollView>
-            </View>
-
+            </ScrollView>
         </View>
+
+
     );
 }
+
+
 const styles = StyleSheet.create({
     TextInput: {
         marginTop: 8,
@@ -124,7 +157,4 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
 
     },
-});
-export function New() {
-    return <WeekDaysCheckbox />;
-}
+})

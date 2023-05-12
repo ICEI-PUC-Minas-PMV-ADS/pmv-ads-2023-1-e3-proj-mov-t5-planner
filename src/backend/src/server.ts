cca475602992575b1d4230461fccdb74995c739a
Walber <walberg.s48@gmail.com>
2023-05-11 23:37:26 -0300

    add backend

---
+++ src/backend/src/server.ts
@@ -0,0 +1,18 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from './lib/routes'
+
+const app = Fastify()
+
+app.register(cors)
+app.register(appRoutes)
+
+app.listen({
+  port: 3333,
+}).then( () => {
+  console.log('Server tá funfando')
+} )
+
+// app.get('/', () => {
+//   return 'Por favor, funcione...'
+// })
--- src/planner/src/screens/Home.tsx
+++ src/planner/src/screens/Home.tsx
@@ -14,6 +14,7 @@ const diasParaPreencherTela = proximosDias - datesFromYearStart.length
 export function Home(){
 
     const { navigate } = useNavigation();
+
     return(
         <View className="flex-1 bg-background px-8 pt-16" >
             <Header />
